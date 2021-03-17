0de5e270e42e25bb2b2e181a6d9541802d90985c
Simone Mainardi <mainardi@ntop.org>
2021-03-17 18:24:46 +0100

    Implements base ControlGroups

--- include/ntop_includes.h
+++ include/ntop_includes.h
@@ -260,6 +260,7 @@ using namespace std;
 #include "LdapAuthenticator.h"
 #endif
 #endif
+#include "ControlGroups.h"
 #include "HostPoolStats.h"
 #include "HostPools.h"
 #include "Fingerprint.h"
---
+++ src/ControlGroups.cpp
@@ -0,0 +1,72 @@
+/*
+ *
+ * (C) 2015-21 - ntop.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "ntop_includes.h"
+
+/* *************************************** */
+
+ControlGroups::ControlGroups() {
+  init_tstamp = time(NULL);
+  member_groups = NULL;
+  loadConfiguration();
+}
+
+/* *************************************** */
+
+ControlGroups::~ControlGroups() {
+}
+
+/* *************************************** */
+
+void ControlGroups::loadGroupMember(u_int group_id, const char * const member) {
+}
+
+/* *************************************** */
+
+void ControlGroups::loadGroupDisabledFlowAlert(u_int group_id, FlowAlertType disabled_flow_alert_type) {
+}
+
+/* *************************************** */
+
+void ControlGroups::loadConfiguration() {
+}
+
+/* *************************************** */
+
+bool ControlGroups::checkChange(time_t *last_change) const {
+  bool changed = false;
+
+  if(*last_change < init_tstamp)
+    changed = true, *last_change = init_tstamp;
+
+  return changed;
+}
+
+/* *************************************** */
+
+Bitmap ControlGroups::getDisabledFlowAlertsBitmap(Host *h) const {
+  Bitmap res;
+
+  return res;
+}
+
+
+/* *************************************** */
